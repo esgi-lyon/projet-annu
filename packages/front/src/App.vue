74f93d1dbf441163b49378a6d9db84a83e518e44
loic-roux <69loloro10@gmail.com>
2021-06-30 01:38:21 +0200

    feat: added vuetify / calendar deps

--- packages/front/src/App.vue
+++ packages/front/src/App.vue
@@ -1,35 +1,23 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/" class="navbar-brand">Home</router-link>
-      <ul>
-        <li>
-          <router-link to="/list">Liste</router-link>
-        </li>
-        <li>
-          <router-link to="/add">Add</router-link>
-        </li>
-      </ul>
-    </nav>
-
-    <div class="container">
-      <router-view />
-    </div>
-  </div>
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
-<script setup>
+<script>
+import HelloWorld from './components/HelloWorld.vue'
 
-</script>
+export default {
+  name: 'App',
 
-<style>
+  components: {
+    HelloWorld,
+  },
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  data: () => ({
+    //
+  }),
 }
-</style>
+</script>
--- packages/front/src/main.js
+++ packages/front/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
+import vuetify from './plugins/vuetify'
 import App from './App.vue'
-import router from "./router";
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(vuetify)
+
+app.mount('#app')
