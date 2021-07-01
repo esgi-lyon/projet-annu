be4e97f6d58e059cc311eb961ce6323d01f95796
loic-roux <69loloro10@gmail.com>
2021-07-01 20:09:05 +0200

    fix: vue

--- packages/front/src/views/FOCourseShow.vue
+++ packages/front/src/views/FOCourseShow.vue
@@ -6,7 +6,7 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import FOCourseDetails from "../components/FOCourseDetails"
+import FOCourseDetails from "../components/FOCourseDetails.vue"
 
 export default defineComponent({
     name: "Add",
